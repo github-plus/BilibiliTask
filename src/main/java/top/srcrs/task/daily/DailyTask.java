60ef518c6717f7232f100bf0309b81cb5cb203d8
cxl <31118423+ChenXueLi0109@users.noreply.github.com>
2022-05-17 09:37:30 +0800

    Update DailyTask.java

--- src/main/java/top/srcrs/task/daily/DailyTask.java
+++ src/main/java/top/srcrs/task/daily/DailyTask.java
@@ -26,6 +26,7 @@ public class DailyTask implements Task {
     public void run() {
         try {
             JSONObject reward = getReward();
+            log.info(reward.toJSONString());
             /* 今天是否完成分享视频任务 */
             boolean isShare = reward.getBoolean("share");
             /* 今天是否完成观看视频任务 */
